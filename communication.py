5eed7e16dd65c6ceae619d59d6f254b3fa8d5994
Corinna Baumgartner <corinna.baumgartner@edu.fh-joanneum.at>
2022-01-26 11:07:26 +0100

    update 260122 11AM

--- communication.py
+++ communication.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 
 # libraries
@@ -5,50 +6,94 @@ import threading
 import queue
 import socket
 import serial
-import RPi.GPIO as GPIO
+import struct
+import binascii
+from  rplidar import RPLidar
 
-# setup for pwm for lidar motor
-lidar_pin = 12 # pin for the pwm signal output
-lidar_frequency = 25 # frequency of the pwm signal
-lidar_duty = 100 # dutycycle of the pwm signal
-GPIO.setwarnings(False)	# disable warnings
-GPIO.setmode(GPIO.BOARD) # set pin numbering system
-GPIO.setup(lidar_pin,GPIO.OUT) # define pin as output
-pi_pwm = GPIO.PWM(lidar_pin,lidar_frequency) # create PWM instance with frequency 25Hz
-pi_pwm.start(0) # start up pwm with dutycycle 0
+#Serial Port Names from Xbee and Lidar USB
+# defined in udev rule 10 for finding the right USB port for the Lidar and the USB
+PORT_LIDAR = '/dev/ttyUSB_LIDAR'
+PORT_XBEE = '/dev/ttyUSB_XBEE'
 
-# queue for UART-Data
-q = queue.Queue(maxsize=2)
+#define lidar element to use the lidar sensor
+lidar = RPLidar(PORT_LIDAR)
+# queue for UART-Data from lidar and xbee
+q = queue.Queue(maxsize=22)
+data = []
 
-# Threads
+### --- Threads for the multithreading --- ###
+
 # get data from lidar-sensor
 def get_lidar():
-    while True:
-        ser = serial.Serial('/dev/ttyS0') # UART-GPIO pins on Raspberry ttyAMA0
-        s = bytes(ser.read(100)) # bytes(ser.read())
-        ##----Verarbeitung lidardaten hier----##
+    print('lidar__')
+    ##----Verarbeitung lidardaten hier----##
+    for measurment in lidar.iter_measurments():
+        if measurment[1] == 0:
+            dist = 8000
+        else:           
+            dist = int(measurment[3]) 
+        deg = int(measurment[2])
+        #print('degrees ')
+        #print(deg)
+        #print('distance  ')
+        #print(dist)
+        #print(type(measurment[0]))
+        s = struct.pack('>hh',deg,dist)     
         print('lidar', s)
         q.put(s)
-        ser.close()
-
+ 
 # get data from xbee antennas
-#def get_xbee():
-   # while True:
-       # ser = serial.Serial('/dev/ttyUSB0') #fixed USB-Port because only one USB device
-       # s = bytes(ser.read(22))
-       # print('xbee', s)
-        #q.put(s)
-        #ser.close()
+def get_xbee():
+    while True:
+        ser = serial.Serial(PORT_XBEE,baudrate = 9600) #fixed USB-Port because only one USB device
+            
+        s = bytes(ser.read(22))
+        print('xbee', s)
+        q.put(s)
+        ser.close()
 
 # put the data from lidar and xbee together and send it to the plc
 def do_mixer(conn):
     while True:
+        print('mixer__')
         s = q.get()
         print('mixer', s)
         #conn.send(s)
-        q.task_done()
+        #q.task_done()
 
-# sent data form plc to xbee antenna
+
+
+# put queue data together
+q.join()
+
+# connect socket for ethernet communication
+ethconn = socket.socket()
+#ethconn.connect(('10.36.37.21', 4711)) #IP-Adress of Raspi and Portnumber. Portnumber must be the same as in PLC programm
+
+# make threads
+xbee_thread = threading.Thread(target=get_xbee, daemon=True)
+lidar_thread = threading.Thread(target=get_lidar, daemon=True)
+mixer_thread = threading.Thread(target=do_mixer, args=(ethconn,), daemon=True)
+#plc_thread = threading.Thread(target=sent_xbee, args=(ethconn,), daemon=True)
+
+
+# start threads
+xbee_thread.start()
+lidar_thread.start()
+mixer_thread.start()
+#plc_thread.start()
+
+
+# put threads together
+xbee_thread.join()
+lidar_thread.join()
+mixer_thread.join()
+#plc_thread.join()
+
+
+
+
+# sent data form plc to xbee antenna for the control panel which we can forget lol
 #def sent_xbee(conn)
 #   while True:
 #        s = conn.recv()
@@ -56,40 +101,3 @@ def do_mixer(conn):
 #        ser.write(s)
 #        print('xbee_s', s)
 #        ser.close()
-
-# pwm signal for lidar motor
-#def do_pwm():
-#    while True:
-#        pi_pwm.ChangeDutyCycle(lidar_duty)
-pi_pwm.ChangeDutyCycle(lidar_duty)
-# put queue data together
-q.join()
-
-# connect socket for ethernet communication
-ethconn = socket.socket()
-#ethconn.connect(('10.36.37.21', 1234)) #IP-Adress of Raspi and Portnumber. Portnumber must be the same as in PLC programm
-
-# make threads
-#xbee_thread = threading.Thread(target=get_xbee, daemon=True)
-lidar_thread = threading.Thread(target=get_lidar, daemon=True)
-mixer_thread = threading.Thread(target=do_mixer, args=(ethconn,), daemon=True)
-#plc_thread = threading.Thread(target=sent_xbee, args=(ethconn,), daemon=True)
-#pwm_thread = threading.Thread(target=do_pwm, daemon=True)
-
-# start threads
-#xbee_thread.start()
-lidar_thread.start()
-mixer_thread.start()
-#plc_thread.start()
-#pwm_thread.start()
-
-# put threads together
-#xbee_thread.join()
-lidar_thread.join()
-mixer_thread.join()
-#plc_thread.join()
-#pwm_thread.join()
-
-### important for future use ###
-# when using 2 or more devices on the USB-Ports you have to find out which device is which...
-# because the raspberry pi uses ttyUSBx (x = 0...3) depending on which device it finds first.
